d48678bdd969696b87d16df437a8d3b58aa4ca7a
feie9454 <feie9454@gmail.com>
2022-03-05 22:37:07 +0800

    Bug fixed. Add video support.

--- mnt.js
+++ mnt.js
@@ -1,47 +1,45 @@
+//This module is used to access local music files 
+//using FileAccess API and return a array including objects
+//By feie9454 2022/2/17
+
+
+"use strict"
 async function mntdir() {
-    try {
-      const dirHandle = await window.showDirectoryPicker();
-      for await (const entry of dirHandle.values()) {
-        if (entry.kind == "file") {
-          if (validationEnd(entry.name, ".mp3")) {
-            $("#int_").remove()
-            if (num % 2) {
-              $(".list-group").append('<a href="javascript:c(' + num + ')" class="list-group-item list-group-item-action list-group-item-dark waves-effect" id="c'+num+'">' + entry.name + '</a>')
-            } else {
-              $(".list-group").append('<a href="javascript:c(' + num + ')" class="list-group-item list-group-item-action list-group-item-primary waves-effect" id="c'+num+'">'  + entry.name + '</a>')
-            }
-            musiclist.push(entry.name)
-            file_ = await entry.getFile()
-            await musicfilelist.push(file_)
-            var tags = {};
-            jsmediatags.read(file_, {
-              onSuccess: function (tag) {
-                musictagslist.push(tag)
-  
-              },
-              onError: function (error) {
-              }
-            });
-            num++
-          } else if (validationEnd(entry.name, ".lrc")) {
-            $("#int_").remove()
-            if (num % 2) {
-              $(".list-group").append('<a href="javascript:c(' + num + ')" class="list-group-item list-group-item-action list-group-item-dark waves-effect" id="c'+num+'">' + entry.name + '</a>')
-            } else {
-              $(".list-group").append('<a href="javascript:c(' + num + ')" class="list-group-item list-group-item-action list-group-item-primary waves-effect" id="c'+num+'">'  + entry.name + '</a>')
-            }
-            musiclist.push(entry.name)
-            file_ = await entry.getFile()
-            await musicfilelist.push(file_)
-            musictagslist.push("lrc")
-            num++
-          }
+  var musicfilelist = []
+  try {
+    const dirHandle = await window.showDirectoryPicker();
+    for await (const entry of dirHandle.values()) {
+      if (entry.kind == "file") {
+        if (validationEnd(entry.name, ".mp3") || validationEnd(entry.name, ".aac") || validationEnd(entry.name, ".ogg") || validationEnd(entry.name, ".flac")) {
+          let music = new Fmusic(entry.name, entry, "music")
+          //let music = new Object()
+          //music.name = entry.name
+          //music.file = await entry.getFile()
+          //music.type = "music"
+          //jsmediatags.read(music.file, {
+          //  onSuccess: function (tag) {
+          //    music.tag = tag
+          //  },
+          //});
+          musicfilelist.push(music)
+        } else if (validationEnd(entry.name, ".lrc")) {
+          //let music = new Object()
+          //music.name = entry.name
+          //music.file = await entry.getFile()
+          //music.type = "lrc"
+          let music = new Fmusic(entry.name, entry, "lrc")
+          musicfilelist.push(music)
+
+        }else if(validationEnd(entry.name, ".mp4")||validationEnd(entry.name, ".webm")){
+          let music = new Fmusic(entry.name, entry, "video")
+          musicfilelist.push(music)
+
         }
       }
-      loadsuccess()
     }
-    catch (e) {
-      console.error(e);
-    }
+    return musicfilelist
   }
-  
+  catch (e) {
+    console.error(e);
+  }
+}
